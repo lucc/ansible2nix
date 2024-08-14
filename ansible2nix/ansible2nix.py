1a9bad58f6eef8095ab86e1ade96740b434590af
Lucas Hoffmann <luc@artsoftandmore.com>
2024-08-14 11:17:09 +0200

    Format python code

--- ansible2nix/ansible2nix.py
+++ ansible2nix/ansible2nix.py
@@ -1,9 +1,10 @@
+import argparse
 import logging
 import subprocess
-import yaml
-import argparse
-from typing import List
 import sys
+from typing import List
+
+import yaml
 
 log = logging.getLogger()
 log.addHandler(logging.StreamHandler())
@@ -11,36 +12,34 @@ log.addHandler(logging.StreamHandler())
 __version__ = "0.1"
 
 LOG_LEVELS = {
-    logging.getLevelName(level): level for level in [
-        logging.DEBUG, logging.INFO, logging.ERROR
-    ]
+    logging.getLevelName(level): level
+    for level in [logging.DEBUG, logging.INFO, logging.ERROR]
 }
+
 
 def gen_galaxy_url(name, version):
     nname = name.replace(".", "-")
     return f"https://galaxy.ansible.com/download/{nname}-{version}.tar.gz"
 
 
-
 def gen_nix_expression(collection):
     log.debug("Generating nix code")
-    name = collection['name']
-    version = collection['version']
+    name = collection["name"]
+    version = collection["version"]
     tarball_url = gen_galaxy_url(name, version)
     cmd = ["nix-prefetch-url", tarball_url]
-
 
     out = subprocess.check_output(cmd)
     log.debug(out)
     sha = out.decode().strip()
 
-    content = '''
+    content = """
         (builtins.fetchurl {{
             name = "{name}";
             url = "{tarball_url}";
             sha256 = "{checksum}";
         }})
-    '''.format(name=name, tarball_url=tarball_url, checksum=sha)
+    """.format(name=name, tarball_url=tarball_url, checksum=sha)
     return content
 
 
@@ -50,26 +49,26 @@ def gen_nix_expression(collection):
 #     """.format(ansible=ansible_drv,)
 #     pass
 
+
 def main(arguments: List[str] = None):
     if not arguments:
         arguments = sys.argv[1:]
 
     parser = argparse.ArgumentParser(
-        description='Generate MPTCP (Multipath Transmission Control Protocol) stats & plots',
+        description="Generate MPTCP (Multipath Transmission Control Protocol) stats & plots",
         epilog="You can report issues at https://github.com/teto/mptcpanalyzer",
     )
 
-    parser.add_argument(
-        "requirements",
-        help="Path towards the 'requirements.yml'"
-    )
+    parser.add_argument("requirements", help="Path towards the 'requirements.yml'")
     # parser.add_argument('ansible_drv', action='store', default="ansible")
-    parser.add_argument('--version', action='version', version=str(__version__))
+    parser.add_argument("--version", action="version", version=str(__version__))
     parser.add_argument(
-        "--debug", "-d", choices=LOG_LEVELS.keys(),
+        "--debug",
+        "-d",
+        choices=LOG_LEVELS.keys(),
         default=logging.getLevelName(logging.ERROR),
         help="More verbose output, can be repeated to be even more "
-        " verbose such as '-dddd'"
+        " verbose such as '-dddd'",
     )
 
     args = parser.parse_args(arguments)
@@ -80,7 +79,7 @@ def main(arguments: List[str] = None):
         content = yaml.safe_load(fd)
 
         nix_derivations = []
-        for collection in content['collections']:
+        for collection in content["collections"]:
             nix_derivations.append(gen_nix_expression(collection))
 
         # for collection in collections:
@@ -92,9 +91,5 @@ def main(arguments: List[str] = None):
         print(output)
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
